b61dfd277fa993d556e358bb0f11ae3ccb58ed3c
emicon-web <emiconweb@emicon.co.in>
2021-04-28 22:52:32 +0530

    https

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const http = require('http');
 const https = require('https');
 const fs = require('fs');
 require('./config/config');
@@ -11,16 +12,18 @@ require('./startup/routes') (app);
 const httpsOptions = {
     cert : fs.readFileSync('./ssl/adminbalancedwellbeing.com.crt','utf8'),
     ca:fs.readFileSync('./ssl/adminbalancedwellbeing.com.ca-bundle','utf8'),
-    key:fs.readFileSync('./ssl/adminbalancedwellbeing_com_key.txt','utf8')
+    key:fs.readFileSync('./ssl/adminbalancedwellbeing_com.key','utf8')
 }
-console.log(httpsOptions.cert);
+
+const httpServer = http.createServer(app);
 const httpsServer = https.createServer(httpsOptions, app);
 
-// app.listen( process.env.PORT,err=>{
-//     if(err){console.log(err);}
-//     console.log(`Server Started On Port : ${process.env.PORT}`);
-// })
-https.createServer(httpsOptions, app).listen(process.env.PORT,err=>{
-        if(err){console.log(err);}
-        console.log(`Server Started On Port : ${process.env.PORT}`);
-    });
+httpServer.listen( process.env.PORT,err=>{
+    if(err){console.log(err);}
+    console.log(`Http Server Started On Port : ${process.env.PORT}`);
+})
+
+httpsServer.listen(process.env.PORT_HTTPS, err=>{
+    if(err){console.log(err);}
+    console.log(`Https Server Started On Port : ${process.env.PORT_HTTPS}`);
+});
