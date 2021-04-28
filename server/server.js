d086e6e4a771b84ad8cd72fcd1ef6dccadaa545b
emicon-web <emiconweb@emicon.co.in>
2021-04-28 19:01:58 +0530

    htf

--- server/server.js
+++ server/server.js
@@ -8,18 +8,19 @@ require('./startup/prod') (app);
 require('./models/db');
 
 require('./startup/routes') (app);
-// const httpsOptions = {
-//     cert : fs.readFileSync('./ssl/adminbalancedwellbeing.com.crt','utf8'),
-//     ca:fs.readFileSync('./ssl/adminbalancedwellbeing.com.ca-bundle','utf8'),
-//     key:fs.readFileSync('./ssl/adminbalancedwellbeing_com_key.txt','utf8')
-// }
-// const httpsServer = https.createServer(httpsOptions, app);
+const httpsOptions = {
+    cert : fs.readFileSync('./ssl/adminbalancedwellbeing.com.crt','utf8'),
+    ca:fs.readFileSync('./ssl/adminbalancedwellbeing.com.ca-bundle','utf8'),
+    key:fs.readFileSync('./ssl/adminbalancedwellbeing_com_key.txt','utf8')
+}
+console.log(httpsOptions.cert);
+const httpsServer = https.createServer(httpsOptions, app);
 
-app.listen( process.env.PORT,err=>{
-    if(err){console.log(err);}
-    console.log(`Server Started On Port : ${process.env.PORT}`);
-})
-// https.createServer(httpsOptions, app).listen(process.env.PORT,err=>{
-//         if(err){console.log(err);}
-//         console.log(`Server Started On Port : ${process.env.PORT}`);
-//     });
+// app.listen( process.env.PORT,err=>{
+//     if(err){console.log(err);}
+//     console.log(`Server Started On Port : ${process.env.PORT}`);
+// })
+https.createServer(httpsOptions, app).listen(process.env.PORT,err=>{
+        if(err){console.log(err);}
+        console.log(`Server Started On Port : ${process.env.PORT}`);
+    });
