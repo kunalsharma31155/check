b5c64bf14a87f062c6e02e92a2592c47cc263e74
emicon-web <emiconweb@emicon.co.in>
2021-04-28 19:46:47 +0530

    ok

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   name: "(PROD)",
   // apiBaseUrl: "http://localhost:5000"
   // apiBaseUrl: "http://3.21.230.235:5000"
-  apiBaseUrl: "https://3.140.223.79:5000"
+  apiBaseUrl: "http://3.140.223.79:5000"
 }
